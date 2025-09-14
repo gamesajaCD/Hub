aae4fafab9b4c61b5f2581b1eab7d3c24db0df93
gamesajaCD <gamesaja650@gmail.com>
2025-09-15 01:28:53 +0800

    updt backup

--- Hub-Backup.lua
+++ Hub-Backup.lua
@@ -1,24 +1,28 @@
 if not game:IsLoaded() then game.Loaded:Wait() end
     if game.PlaceId == 18629053284 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/udinInAllWorld/PrivateScript/main/18629053284.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/gamesajaCD/PrivateScript/main/18629053284.lua"))()
     elseif game.PlaceId == 18629058177 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/udinInAllWorld/PrivateScript/main/18629058177.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/gamesajaCD/PrivateScript/main/18629058177.lua"))()
     elseif game.PlaceId == 16572568747 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/udinInAllWorld/PrivateScript/main/16572568747.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/gamesajaCD/PrivateScript/main/16572568747.lua"))()
     elseif game.PlaceId == 10534865425 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/udinInAllWorld/PrivateScript/main/10534865425.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/gamesajaCD/PrivateScript/main/10534865425.lua"))()
     elseif game.PlaceId == 13127800756 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/udinInAllWorld/PrivateScript/main/13127800756.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/gamesajaCD/PrivateScript/main/13127800756.lua"))()
     elseif game.PlaceId == 18401171146 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/udinInAllWorld/PrivateScript/main/18401171146.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/gamesajaCD/PrivateScript/main/18401171146.lua"))()
     elseif game.PlaceId == 18629058177 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/udinInAllWorld/PrivateScript/main/18629058177.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/gamesajaCD/PrivateScript/main/18629058177.lua"))()
     elseif game.PlaceId == 92039548740735 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/udinInAllWorld/PrivateScript/main/92039548740735.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/gamesajaCD/PrivateScript/main/92039548740735.lua"))()
     elseif game.PlaceId == 89438510123061 then -- survival underwolrd
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/udinInAllWorld/PrivateScript/main/89438510123061.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/gamesajaCD/PrivateScript/main/89438510123061.lua"))()
 	elseif game.PlaceId == 18629067363 then -- survival nopvp
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/udinInAllWorld/PrivateScript/main/18629067363.lua"))()
-    else
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/gamesajaCD/PrivateScript/main/18629067363.lua"))()
+    elseif game.PlaceId == 76260928389356 then -- Guitar Simulator
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/gamesajaCD/PrivateScript/main/76260928389356.lua"))()
+	elseif game.PlaceId == 81257648942232 then -- Slash Mob Simulator
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/gamesajaCD/PrivateScript/main/81257648942232.lua"))()
+	else
       print("Game isn't Supported")
 end
